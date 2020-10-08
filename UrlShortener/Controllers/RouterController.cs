2bfb3c81067eb6119feaf5cb3691a49570b6e5b8
Kerem Dincer <b.keremdincer@gmail.com>
2020-10-08 17:43:05 +0300

    Redirector endpoint added.

---
+++ UrlShortener/Controllers/RouterController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UrlShortener.Controllers
+{
+    [Route("")]
+    public class RouterController : Controller
+    {
+        [Route("{token}")]
+        [HttpGet]
+        public IActionResult Index(string token)
+        {
+            return Redirect("https://www.youtube.com");
+        }
+    }
+}
