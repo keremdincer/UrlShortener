cf64e38752dee81988c2b97402580d55ae543d2f
Kerem Dincer <b.keremdincer@gmail.com>
2020-10-08 18:09:45 +0300

    FIX: Entity connection - Table name is forced singular. - Field is set nullable.

--- UrlShortener/Data/ShortUrl.cs
+++ UrlShortener/Data/ShortUrl.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace UrlShortener.Data
 {
+    [Table("ShortUrl")]
     public class ShortUrl
     {
         public int Id { get; set; }
         public string OriginalUrl { get; set; }
         public string Url { get; set; }
         public bool isPermanent { get; set; }
-        public int CreatorId { get; set; }
+        public int? CreatorId { get; set; }
         public int ApiKeyId { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime ExpiresAt { get; set; }
