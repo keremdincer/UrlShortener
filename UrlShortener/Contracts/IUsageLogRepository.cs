a5210bf6b893bcbf04005c9f903f53cff67a28a6
Kerem Dincer <b.keremdincer@gmail.com>
2020-10-08 17:43:36 +0300

    Repository interfaces are defined.

---
+++ UrlShortener/Contracts/IUsageLogRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UrlShortener.Data;
+
+namespace UrlShortener.Contracts
+{
+    public interface IUsageLogRepository : IRepositoryBase<UsageLog>
+    {
+    }
+}
