7ac04a5d0a7c574ca08da74de2d94db6ded1ffb7
Kerem Dincer <b.keremdincer@gmail.com>
2020-10-09 18:06:24 +0300

    NanoId generator service is implemented.

---
+++ UrlShortener/Services/NanoIdGenerator.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Nanoid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UrlShortener.Contracts;
+
+namespace UrlShortener.Services
+{
+    public class NanoIdGenerator : INanoIdGenerator
+    {
+        private readonly IConfiguration _config;
+
+        public NanoIdGenerator(IConfiguration config) => _config = config;
+
+        public string Generate()
+        {
+            var nanoid = Nanoid.Nanoid.Generate(
+                _config["NanoId:Domain"],
+                int.Parse(_config["NanoId:Length"])
+            );
+
+            return nanoid;
+        }
+    }
+}
