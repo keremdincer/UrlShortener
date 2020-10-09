04aeb10bf752dfe582ba69cc93b5d44d15e37755
Kerem Dincer <b.keremdincer@gmail.com>
2020-10-09 18:10:07 +0300

    RouterController is implemented.

--- UrlShortener/Controllers/RouterController.cs
+++ UrlShortener/Controllers/RouterController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using UrlShortener.Contracts;
 
@@ -17,11 +18,31 @@ namespace UrlShortener.Controllers
             _shortUrlRepository = shortUrlRepository;
         }
 
+        /// <summary>
+        /// Kısa url'lerin yönlendirmesini yapar.
+        /// </summary>
+        /// <param name="token">Kısa url kodu</param>
+        /// <remarks>
+        /// Not: Bu method documentation sayfası üzerinde yönlendirme yapmaz.
+        /// </remarks>
+        /// <returns></returns>
+        /// <response code="308">Kısa url geçerli ise yönlendirme yapar.</response>
+        /// <response code="404">Karşılıksız bir kısa url ise</response>
         [Route("{token}")]
+        [ProducesResponseType(StatusCodes.Status308PermanentRedirect)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         [HttpGet]
-        public IActionResult Index(string token)
+        public async Task<IActionResult> Index(string token)
         {
-            return Redirect("https://www.youtube.com");
+            var shortUrl = await _shortUrlRepository.FindByToken(token);
+            if (shortUrl == null)
+                return NotFound();
+
+            // Check if short url's expiration
+            if (shortUrl.ExpiresAt.HasValue && shortUrl.ExpiresAt.Value < DateTime.Now)
+                return NotFound();
+
+            return Redirect(shortUrl.OriginalUrl);
         }
     }
 }
